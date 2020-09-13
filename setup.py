82f5206d3cdaa0c600266a521e7515b3eb1f5e1d
Epic <admin@itzepic.net>
2020-09-13 15:50:16 +0200

    Fix setup files

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@
 Created by Epic at 9/4/20
 """
 from setuptools import setup
-from speedcord.values import version
+
+with open('speedcord/values.py') as f:
+    version = re.search(r'^version\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setup(
     name='speedcord',
@@ -11,6 +13,6 @@ setup(
     url='https://github.com/tag-epic/speedcord',
     license='MIT',
     author='Epic',
-    install_requires=open("requirements.txt").readlines(),
+    install_requires=["aiohttp"],
     description='A simple lightweight discord library'
 )
