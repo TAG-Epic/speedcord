229ea29709094863818716ea3d527fb5194b6529
TAG-Epic <admin@itzepic.net>
2020-09-13 22:34:49 +0200

    Use @listen in example

--- examples/simplecommand.py
+++ examples/simplecommand.py
@@ -8,15 +8,13 @@ from speedcord.http import Route
 client = speedcord.Client(intents=512)
 
 
+@client.listen("MESSAGE_CREATE")
 async def on_message(data, shard):
     message = data
     if message["content"].lower() == "!test":
         channel = message["channel_id"]
         route = Route("POST", f"/channels/{channel}/messages", channel_id=channel)
         await client.http.request(route, json={"content": "Hello world!"})
-
-
-client.event_dispatcher.register("MESSAGE_CREATE", on_message)
 
 client.token = "token"
 client.run()
