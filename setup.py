d001b681286e54558583ef9a56f3e51f74902671
Epic <admin@itzepic.net>
2020-09-13 15:51:35 +0200

    Good idea to import re?

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 Created by Epic at 9/4/20
 """
 from setuptools import setup
+import re
 
 with open('speedcord/values.py') as f:
     version = re.search(r'^version\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
