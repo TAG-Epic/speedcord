edfaabd89e3e6ab93977687860edfbdfc1086370
TAG-Epic <admin@itzepic.net>
2020-09-28 15:09:22 +0200

    Update example to use env vars for token & enable logging by default

--- examples/simplecommand.py
+++ examples/simplecommand.py
@@ -4,8 +4,11 @@ Created by Epic at 9/5/20
 
 import speedcord
 from speedcord.http import Route
+from os import environ as env
+from logging import basicConfig, DEBUG
 
-client = speedcord.Client(intents=512)
+client = speedcord.Client(intents=512, shard_count=5)
+basicConfig(level=DEBUG)  # Comment this out if you don't want to see what's going on behind the scenes
 
 
 @client.listen("MESSAGE_CREATE")
@@ -16,5 +19,5 @@ async def on_message(data, shard):
         route = Route("POST", f"/channels/{channel}/messages", channel_id=channel)
         await client.http.request(route, json={"content": "Hello world!"})
 
-client.token = "token"
+client.token = env["TOKEN"]
 client.run()
