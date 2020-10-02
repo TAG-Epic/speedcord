ab038ab3f9605ffbc944686a79f225d74ce1a7c7
Epic <tagepicuwu@gmail.com>
2020-10-02 22:34:38 +0200

    add speedcord.__version__ alias for speedcord.values.version

--- speedcord/__init__.py
+++ speedcord/__init__.py
@@ -4,4 +4,5 @@ Created by Epic at 9/1/20
 Simple library to interact with the discord API
 """
 from .client import Client
-__all__ = ("Client", )
+from .values import version as __version__
+__all__ = ("Client", "__version__")
