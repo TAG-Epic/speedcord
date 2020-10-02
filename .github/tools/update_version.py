860bcc6aed127b0ea89dba5546bb61484eabc91f
Epic <tagepicuwu@gmail.com>
2020-10-02 22:48:27 +0200

    PyPI already does hash checking

--- .github/tools/update_version.py
+++ .github/tools/update_version.py
@@ -6,30 +6,6 @@ from subprocess import check_output
 import hashlib
 
 version_file = Path("speedcord/values.py")
-last_hash_file = Path(".github/tools/last_publish_hash.hash")
-
-with last_hash_file.open("r+") as f:
-    last_hash = f.read()
-
-
-def get_hash(file):
-    hash_md5 = hashlib.md5()
-    with file.open("rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
-
-dist_path = Path("dist/")
-
-dist_output = list(dist_path.glob("*"))[0]
-
-new_hash = get_hash(dist_output)
-if last_hash == new_hash:
-    exit(0)
-with last_hash_file.open("w+") as f:
-    f.write(new_hash)
-
 last_commit = check_output(["git", "log", "-1", "--pretty=%B"]).decode("utf-8")
 
 is_major = last_commit.lower().startswith("major:")
@@ -52,7 +28,6 @@ for line in lines:
         new_lines.append(new_version)
     else:
         new_lines.append(line)
-new_lines.append("\n")
 
 with version_file.open("w") as f:
     f.writelines(new_lines)
