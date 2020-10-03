af609cba39e29f3097343041fb608f4f127ee725
TAG-Epic <admin@itzepic.net>
2020-10-03 19:24:59 +0200

    Spaces begone

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ from setuptools import setup, find_packages
 import re
 
 with open('speedcord/values.py') as f:
-    version = re.search(r'^version\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+	version = re.search(r'^version\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setup(
-    name='speedcord',
-    version=version,
-    packages=find_packages(),
-    url='https://github.com/tag-epic/speedcord',
-    license='MIT',
-    author='Epic',
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    install_requires=["aiohttp", "ujson"],
-    description='A simple lightweight discord library',
-    python_requires='>=3.7',
+	name='speedcord',
+	version=version,
+	packages=find_packages(),
+	url='https://github.com/tag-epic/speedcord',
+	license='MIT',
+	author='Epic',
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	install_requires=["aiohttp", "ujson"],
+	description='A simple lightweight discord library',
+	python_requires='>=3.7',
 )
