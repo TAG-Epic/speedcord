095c76257fa9b5dbbd1ae6ca9ebb776e44ecd127
TAG-Epic <admin@itzepic.net>
2020-11-19 18:01:58 +0100

    Don't use * imports

--- speedcord/__init__.py
+++ speedcord/__init__.py
@@ -4,7 +4,8 @@ Created by Epic at 9/1/20
 Simple library to interact with the discord API
 """
 from .client import Client
-from .exceptions import *
+from .exceptions import HTTPException, Forbidden, NotFound, Unauthorized, LoginException, InvalidToken, \
+    ConnectionsExceeded, GatewayException, GatewayClosed, GatewayUnavailable
 from .values import version as __version__
 
 __all__ = ("__version__", "Client", "HTTPException", "Forbidden", "NotFound",
